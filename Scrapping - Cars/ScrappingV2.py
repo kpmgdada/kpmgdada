60b4954283165538a98e2fc3e177c108566ea0c4
makdaddy604 <alanmak@gmail.com>
2019-08-23 17:17:46 -0400

    infrastructure

---
+++ Scrapping - Cars/ScrappingV2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 12 14:30:50 2019
+
+@author: alanmak
+"""
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+import time
+
+import pandas as pd
